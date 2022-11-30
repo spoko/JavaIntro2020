bebbbc8859ab1a26bf1b9dbdcdb6501da400a8bf
Ibraim Asanov <iasanov@nemetschek.bg>
2022-11-30 20:55:34 +0200

    Added main method and some Console interactions

--- src/main/java/programming/intro/Intro.java
+++ src/main/java/programming/intro/Intro.java
@@ -1,5 +1,32 @@
+//package i.e. location of the file
 package programming.intro;
 
-public class Intro {
+import java.util.Scanner;
 
+//Class
+public class Intro {
+    //Main method of the class
+    public static void main(String[] args) {
+        //outputs the text in the console
+        System.out.println("Please enter your name:");
+        //scanner class used for getting the user input
+        Scanner scanner = new Scanner(System.in);
+        //variable name for storing the name entered
+        String name = scanner.nextLine();
+
+        //outputs the text in the console
+        System.out.println("Please enter your age:");
+        //variable age for storing the age entered
+        int age = scanner.nextInt();
+
+        //outputs the text in the console
+        System.out.println("Hi, " + name + " your age is: " + age);
+
+        //checks some logic
+        if (age > 17){
+            System.out.println(name + " is able to drink.");
+        }else {
+            System.out.println(name + " is NOT able to drink");
+        }
+    }
 }
